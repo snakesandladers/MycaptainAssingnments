0c2ebc0f1244d3a946acc7ef9aa4ef6e327a9038
snakesandladers <93540215+snakesandladers@users.noreply.github.com>
2021-12-16 14:21:33 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,17 @@
+list1=[12,-7,5,64,-14]
+List2=[12,14,-95,3]
+for num in list1:
+    if num>=0:
+        print(num,end="   ")
+for num in List2:
+    if num>=1:
+        print(num,end="   ")
+        
+ 
+
+
+
+
+
+
+
