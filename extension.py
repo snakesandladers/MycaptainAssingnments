55dd7cdee81db8c62ad5fac7c3d7ea3031a6315c
snakesandladers <93540215+snakesandladers@users.noreply.github.com>
2021-12-16 08:41:02 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,6 +1,6 @@
 FileName=input("write the filename ")
 File_extns=FileName.split(".")
-print("the extension is"+repr(File_extns[-1]))
+print("the extension is"+(File_extns[-1]))
 
 output:
   write the filename project.py
